190cb86761772bb19b0ec08bec6f1c1cf4732d46
Alex Russell <alexrusselldev@gmail.com>
2023-11-28 15:54:10 +0000

    feat: ✨ add total mileage to lifetime stats

--- src/collections/Vehicles/fields/vehicleStats.tsx
+++ src/collections/Vehicles/fields/vehicleStats.tsx
@@ -10,7 +10,9 @@ interface IRefuelsResponse {
 const VehicleStatsField: React.FC<Props> = ({ path }) => {
   const [totals, setTotals] = useState<Record<string, any>>();
   const [refuels, setRefuels] = useState<IRefuelsResponse>();
+  const [firstRefuel, setFirstRefuel] = useState<IRefuelsResponse>();
   const { id } = useDocumentInfo();
+
   useEffect(() => {
     const fetchData = async () => {
       const totals = await fetch(
@@ -24,6 +26,11 @@ const VehicleStatsField: React.FC<Props> = ({ path }) => {
 
       setRefuels(await refuels.json());
 
+      const firstRefuel = await fetch(
+        `http://localhost:3000/api/refuels?where[vehicle][equals]=${id}&sort=createdAt&limit=1`
+      );
+
+      setFirstRefuel(await firstRefuel.json());
     };
 
     fetchData();
@@ -34,10 +41,10 @@ const VehicleStatsField: React.FC<Props> = ({ path }) => {
   });
 
   return (
-    <div className="grid grid-cols-10 p-8 md:p-0">
-      <div className="flex flex-col items-center col-span-10 sm:col-span-5">
+    <div className="grid grid-cols-10 p-8 md:p-0 gap-8">
+      <div className="flex flex-col items-center sm:items-start col-span-10 sm:col-span-5 p-8  bg-[#181818]">
         <h2 className="text-3xl w-fit">Lifetime Stats</h2>
-        <div className="flex flex-col items-center sm:grid sm:grid-cols-2 md: grid-cols-3">
+        <div className="flex flex-col w-full items-center sm:grid sm:grid-cols-2 md:grid-cols-3">
           <div className="flex flex-col items-center sm:items-start">
             <p className="font-bold text-4xl mb-0">{totals?.totalCapacity}</p>
             <p>Fuel (Liters)</p>
@@ -47,6 +54,18 @@ const VehicleStatsField: React.FC<Props> = ({ path }) => {
               {currencyFormatter.format(totals?.totalCost)}
             </p>
             <p>Total Fuel Cost</p>
+          </div>
+          <div className="flex flex-col items-center sm:items-start">
+            <p className="font-bold text-4xl mb-0">
+              {refuels?.docs?.[0]?.mileage}
+            </p>
+            <p>Total Miles Driven</p>
+          </div>
+          <div className="flex flex-col items-center sm:items-start">
+            <p className="font-bold text-4xl mb-0">
+              {refuels?.docs?.[0]?.mileage - firstRefuel?.docs?.[0]?.mileage}
+            </p>
+            <p>Miles Since Owned</p>
           </div>
         </div>
       </div>
