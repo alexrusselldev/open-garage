832d37457f3b0afd85ae1effb0e8faa4d7f192e7
Alex Russell <alexrusselldev@gmail.com>
2023-11-28 15:53:41 +0000

    feat: ✨ add recent refuels widget

--- src/collections/Vehicles/fields/vehicleStats.tsx
+++ src/collections/Vehicles/fields/vehicleStats.tsx
@@ -1,11 +1,15 @@
 import { useDocumentInfo } from "payload/dist/admin/components/utilities/DocumentInfo";
+import { Refuel } from "payload/generated-types";
 import React, { useEffect } from "react";
 import { useState } from "react";
 
 type Props = { path: string };
-
+interface IRefuelsResponse {
+  docs: Refuel[];
+}
 const VehicleStatsField: React.FC<Props> = ({ path }) => {
   const [totals, setTotals] = useState<Record<string, any>>();
+  const [refuels, setRefuels] = useState<IRefuelsResponse>();
   const { id } = useDocumentInfo();
   useEffect(() => {
     const fetchData = async () => {
@@ -13,6 +17,13 @@ const VehicleStatsField: React.FC<Props> = ({ path }) => {
         `http://localhost:3000/api/vehicles/${id}/totals`
       );
       setTotals(await totals.json());
+
+      const refuels = await fetch(
+        `http://localhost:3000/api/refuels?where[vehicle][equals]=${id}&sort=-createdAt&limit=3`
+      );
+
+      setRefuels(await refuels.json());
+
     };
 
     fetchData();
@@ -37,6 +48,32 @@ const VehicleStatsField: React.FC<Props> = ({ path }) => {
             </p>
             <p>Total Fuel Cost</p>
           </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col itemc-center sm:items-start col-span-10 sm:col-span-5 p-8 bg-[#181818]">
+        <h2 className="text-3xl ">Recent Refuels</h2>
+        <div className="w-full">
+          {refuels?.docs?.length != undefined &&
+            refuels?.docs?.length > 0 &&
+            refuels.docs.map((refuel: Refuel) => {
+              return (
+                <a
+                  href={`/admin/collections/refuels/${refuel.id}`}
+                  className="no-underline"
+                >
+                  <div className="grid grid-cols-3 w-full hover:bg-[#212121]">
+                    <p className="m-0 p-2 h-fit">{refuel.capacity}</p>
+                    <p className="m-0 p-2">
+                      {currencyFormatter.format(refuel.cost)}
+                    </p>
+                    <p className="m-0 p-2">
+                      {refuel.full ? "Full" : "Not Full"}
+                    </p>
+                  </div>
+                </a>
+              );
+            })}
         </div>
       </div>
     </div>
