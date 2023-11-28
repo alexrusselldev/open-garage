592261ca71d6e046644d3e13b7644b9dc9c845e1
Alex Russell <alexrusselldev@gmail.com>
2023-11-28 15:19:31 +0000

    feat: ✨ reformat lifetime totals data

--- src/collections/Vehicles/fields/vehicleStats.tsx
+++ src/collections/Vehicles/fields/vehicleStats.tsx
@@ -5,23 +5,40 @@ import { useState } from "react";
 type Props = { path: string };
 
 const VehicleStatsField: React.FC<Props> = ({ path }) => {
-  const [stats, setStats] = useState<Record<string, any>>();
+  const [totals, setTotals] = useState<Record<string, any>>();
   const { id } = useDocumentInfo();
   useEffect(() => {
     const fetchData = async () => {
       const totals = await fetch(
         `http://localhost:3000/api/vehicles/${id}/totals`
       );
-      setStats(await totals.json());
+      setTotals(await totals.json());
     };
 
     fetchData();
   }, []);
+  const currencyFormatter = new Intl.NumberFormat("en-UK", {
+    style: "currency",
+    currency: "GBP",
+  });
 
   return (
-    <div>
-      <p>Total Capacity: {stats?.totalCapacity}</p>
-      <p>Total Cost: {stats?.totalCost}</p>
+    <div className="grid grid-cols-10 p-8 md:p-0">
+      <div className="flex flex-col items-center col-span-10 sm:col-span-5">
+        <h2 className="text-3xl w-fit">Lifetime Stats</h2>
+        <div className="flex flex-col items-center sm:grid sm:grid-cols-2 md: grid-cols-3">
+          <div className="flex flex-col items-center sm:items-start">
+            <p className="font-bold text-4xl mb-0">{totals?.totalCapacity}</p>
+            <p>Fuel (Liters)</p>
+          </div>
+          <div className="flex flex-col items-center sm:items-start">
+            <p className="font-bold text-4xl mb-0">
+              {currencyFormatter.format(totals?.totalCost)}
+            </p>
+            <p>Total Fuel Cost</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
