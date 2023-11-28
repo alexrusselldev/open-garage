bb8291f7f9bc689c330db26e92b6fb48a9213a10
Alex Russell <alexrusselldev@gmail.com>
2023-11-28 09:24:29 +0000

    feat: ✨ add fields for numerical data

--- src/collections/Refuels/Refuels.ts
+++ src/collections/Refuels/Refuels.ts
@@ -1,26 +1,38 @@
 import payload from "payload";
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig, Condition } from "payload/types";
 import { collectionBeforeChange } from "./hooks/collectionHooks";
-import { vehicleDisplayAfterRead } from "./hooks/fieldHooks";
+import {
+  displayFieldAfterRead,
+  vehicleDisplayAfterRead,
+} from "./hooks/fieldHooks";
+
+const isReadOnly: Condition<any, any> = (data) => {
+  return data.isReadOnly;
+};
+
+const isNotReadOnly: Condition<any, any> = (data) => {
+  return !data.isReadOnly;
+};
 
 const Refuels: CollectionConfig = {
   slug: "refuels",
   admin: { useAsTitle: "fullTitle" },
   hooks: { beforeChange: [collectionBeforeChange] },
   fields: [
+    // admin field for controlling write-once logic
     {
       name: "isReadOnly",
       type: "checkbox",
       admin: { hidden: true },
       defaultValue: false,
     },
+
+    // vehicle relationship fields
     {
       name: "vehicle",
       type: "relationship",
       admin: {
-        condition: (data) => {
-          return !data.isReadOnly;
-        },
+        condition: isNotReadOnly,
       },
       relationTo: "vehicles",
       required: true,
@@ -32,13 +44,78 @@ const Refuels: CollectionConfig = {
       type: "text",
       admin: {
         readOnly: true,
-        condition: (data) => {
-          return data.isReadOnly;
-        },
+        condition: isReadOnly,
       },
       hooks: {
         afterRead: [vehicleDisplayAfterRead],
       },
+    },
+
+    // numerical data fields
+    {
+      type: "row",
+      fields: [
+        {
+          name: "mileage",
+          type: "number",
+          admin: {
+            condition: isNotReadOnly,
+          },
+          required: true,
+        },
+        {
+          name: "mileageDisplay",
+          label: "Mileage",
+          type: "number",
+          admin: {
+            readOnly: true,
+            condition: isReadOnly,
+          },
+          hooks: {
+            afterRead: [displayFieldAfterRead("mileage")],
+          },
+        },
+        {
+          name: "capacity",
+          type: "number",
+          admin: {
+            condition: isNotReadOnly,
+          },
+          required: true,
+        },
+        {
+          name: "capacityDisplay",
+          label: "Capacity",
+          type: "number",
+          admin: {
+            readOnly: true,
+            condition: isReadOnly,
+          },
+          hooks: {
+            afterRead: [displayFieldAfterRead("capacity")],
+          },
+        },
+        {
+          name: "cost",
+          type: "number",
+          admin: {
+            condition: isNotReadOnly,
+          },
+          required: true,
+        },
+        {
+          name: "costDisplay",
+          label: "Cost",
+          type: "number",
+          admin: {
+            readOnly: true,
+            condition: isReadOnly,
+          },
+          hooks: {
+            afterRead: [displayFieldAfterRead("cost")],
+          },
+        },
+      ],
     },
   ],
 };
